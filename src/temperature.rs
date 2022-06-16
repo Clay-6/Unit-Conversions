d9f6536ecc323698fd1b2ead681f597cc997f202
Clayton <claytonwambu@gmail.com>
2022-06-16 18:32:41 +0100

    Added conversion functions between kelvin & celsius

--- src/temperature.rs
+++ src/temperature.rs
@@ -10,6 +10,18 @@ pub fn celsius_to_fahrenheit(celsius: f64) -> f64 {
     celsius * 1.8 + 32.0
 }
 
+/// Converts a temperature in Kelvin to its
+/// equivalent in celsius
+pub fn kelvin_to_celsius(kelvin: f64) -> f64 {
+    kelvin - 273.15
+}
+
+/// Converts a temperature in Celsius to its
+/// equivalent in Kelvin
+pub fn celsius_to_kelvin(celsius: f64) -> f64 {
+    celsius + 273.15
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +42,23 @@ mod tests {
 
         let parity = -40.0;
         assert_eq!(celsius_to_fahrenheit(parity), -40.0);
+    }
+
+    #[test]
+    fn k_to_c() {
+        let abs_zero = 0.0;
+        assert_eq!(kelvin_to_celsius(abs_zero), -273.15);
+
+        let room_temp = 294.15;
+        assert_eq!(kelvin_to_celsius(room_temp), 21.0);
+    }
+
+    #[test]
+    fn c_to_k() {
+        let abs_zero = -273.15;
+        assert_eq!(celsius_to_kelvin(abs_zero), 0.0);
+
+        let room_temp = 21.0;
+        assert_eq!(celsius_to_kelvin(room_temp), 294.15);
     }
 }
