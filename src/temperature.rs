4aa0fd8d07a0cb49b718a69f26b621f43f22c1e2
Clayton <claytonwambu@gmail.com>
2022-06-16 18:35:34 +0100

    Tests now use `approx::assert_relative_eq!` instead of `assert_eq!`

--- src/temperature.rs
+++ src/temperature.rs
@@ -25,40 +25,41 @@ pub fn celsius_to_kelvin(celsius: f64) -> f64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::assert_relative_eq;
 
     #[test]
     fn f_to_c() {
         let abs_zero = -459.67;
-        assert_eq!(fahrenheit_to_celsius(abs_zero), -273.15);
+        assert_relative_eq!(fahrenheit_to_celsius(abs_zero), -273.15);
 
         let parity = -40.0;
-        assert_eq!(fahrenheit_to_celsius(parity), -40.0)
+        assert_relative_eq!(fahrenheit_to_celsius(parity), -40.0)
     }
 
     #[test]
     fn c_to_f() {
         let abs_zero = -273.15;
-        assert_eq!(celsius_to_fahrenheit(abs_zero), -459.67);
+        assert_relative_eq!(celsius_to_fahrenheit(abs_zero), -459.67);
 
         let parity = -40.0;
-        assert_eq!(celsius_to_fahrenheit(parity), -40.0);
+        assert_relative_eq!(celsius_to_fahrenheit(parity), -40.0);
     }
 
     #[test]
     fn k_to_c() {
         let abs_zero = 0.0;
-        assert_eq!(kelvin_to_celsius(abs_zero), -273.15);
+        assert_relative_eq!(kelvin_to_celsius(abs_zero), -273.15);
 
         let room_temp = 294.15;
-        assert_eq!(kelvin_to_celsius(room_temp), 21.0);
+        assert_relative_eq!(kelvin_to_celsius(room_temp), 21.0);
     }
 
     #[test]
     fn c_to_k() {
         let abs_zero = -273.15;
-        assert_eq!(celsius_to_kelvin(abs_zero), 0.0);
+        assert_relative_eq!(celsius_to_kelvin(abs_zero), 0.0);
 
         let room_temp = 21.0;
-        assert_eq!(celsius_to_kelvin(room_temp), 294.15);
+        assert_relative_eq!(celsius_to_kelvin(room_temp), 294.15);
     }
 }
