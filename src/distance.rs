42d2ad5692e4cf54354f70a4f9bd61d9251771c7
Clayton <claytonwambu@gmail.com>
2022-06-18 11:00:45 +0100

    Added tests for Km & miles conversion functions

--- src/distance.rs
+++ src/distance.rs
@@ -3,5 +3,25 @@ pub fn miles_to_km(miles: f64) -> f64 {
 }
 
 pub fn km_to_miles(km: f64) -> f64 {
-    km * 0.62137119
+    km * 0.6213712
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn km_to_mi() {
+        assert_relative_eq!(km_to_miles(1.0), 0.6213712);
+        assert_relative_eq!(km_to_miles(69.0), 42.8746128);
+        assert_relative_eq!(km_to_miles(420.0), 260.975904);
+    }
+
+    #[test]
+    fn mi_to_km() {
+        assert_relative_eq!(miles_to_km(1.0), 1.609344);
+        assert_relative_eq!(miles_to_km(69.0), 111.044736);
+        assert_relative_eq!(miles_to_km(420.0), 675.92448);
+    }
 }
