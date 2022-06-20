68b395b94881b79e97d52a66b5a2bdc483079d60
Clayton <claytonwambu@gmail.com>
2022-06-20 17:23:59 +0100

    Added Oz & g conversion functions

--- src/weight.rs
+++ src/weight.rs
@@ -10,6 +10,18 @@ pub fn kg_to_lbs(kg: f64) -> f64 {
     kg / 0.45359237
 }
 
+/// Convert a value in Ounces to its
+/// equivalent in Grams
+pub fn ounces_to_grams(oz: f64) -> f64 {
+    oz * 28.34952
+}
+
+/// Convert a value in Grams to its
+/// equivalent in Ounces
+pub fn grams_to_ounces(g: f64) -> f64 {
+    g * 0.03527396195
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +39,19 @@ mod tests {
         assert_relative_eq!(kg_to_lbs(0.1), 0.22046226218487758);
         assert_relative_eq!(kg_to_lbs(10.0), 22.046226218487757);
         assert_relative_eq!(kg_to_lbs(100.0), 220.46226218487757);
+    }
+
+    #[test]
+    fn oz_to_g() {
+        assert_relative_eq!(ounces_to_grams(1.0), 28.34952);
+        assert_relative_eq!(ounces_to_grams(10.0), 283.4952);
+        assert_relative_eq!(ounces_to_grams(100.0), 2834.952);
+    }
+
+    #[test]
+    fn g_to_oz() {
+        assert_relative_eq!(grams_to_ounces(1.0), 0.03527396195);
+        assert_relative_eq!(grams_to_ounces(10.0), 0.3527396195);
+        assert_relative_eq!(grams_to_ounces(100.0), 3.527396195);
     }
 }
