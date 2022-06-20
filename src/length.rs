0be047697c84be43226fcb66f54e03570ecfdfaa
Clayton <claytonwambu@gmail.com>
2022-06-20 19:35:17 +0100

    Added Inches & Cm conversion functions

--- src/length.rs
+++ src/length.rs
@@ -12,6 +12,20 @@ pub fn km_to_miles(km: f64) -> f64 {
     km * 0.6213712
 }
 
+/// Convert a value in Inches to its
+/// equivalent in Centimeters
+#[inline]
+pub fn inches_to_cm(inches: f64) -> f64 {
+    inches * 2.54
+}
+
+/// Convert a value in Centimeters to its
+/// equivalent in Inches
+#[inline]
+pub fn cm_to_inches(cm: f64) -> f64 {
+    cm / 2.54
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,5 +43,19 @@ mod tests {
         assert_relative_eq!(miles_to_km(1.0), 1.609344);
         assert_relative_eq!(miles_to_km(69.0), 111.044736);
         assert_relative_eq!(miles_to_km(420.0), 675.92448);
+    }
+
+    #[test]
+    fn in_to_cm() {
+        assert_relative_eq!(inches_to_cm(1.0), 2.54);
+        assert_relative_eq!(inches_to_cm(10.0), 25.40);
+        assert_relative_eq!(inches_to_cm(100.0), 254.0);
+    }
+
+    #[test]
+    fn cm_to_in() {
+        assert_relative_eq!(cm_to_inches(1.0), 0.39370078740157477);
+        assert_relative_eq!(cm_to_inches(10.0), 3.9370078740157477);
+        assert_relative_eq!(cm_to_inches(100.0), 39.370078740157477);
     }
 }
